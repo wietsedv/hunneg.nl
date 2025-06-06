7d032314387bea9464860d8c4f66e266abebd078
Wietse de Vries <wietsedv@proton.me>
2025-06-06 15:37:23 +0200

    better manual input

--- pages/betalen/index.vue
+++ pages/betalen/index.vue
@@ -12,14 +12,27 @@ const href = computed(
 <template>
   <div class="max-w-xs mx-auto">
     <div class="my-8 flex bg-white border-gray-400 shadow-sm border-1 rounded-md text-2xl leading-16 overflow-hidden">
-      <span v-if="amount > 1" class="flex-2/12 text-center cursor-pointer select-none hover:bg-gray-100 transition-colors" v-on:click="amount -= 1">-</span>
-      <span v-else class="flex-3/12"></span>
+      <span v-if="amount == 1" class="flex-3/12"></span>
+      <span v-else-if="!Number.isInteger(amount)" class="flex-2/12"></span>
+      <span
+        v-else
+        class="flex-2/12 text-center cursor-pointer select-none hover:bg-gray-100 transition-colors"
+        v-on:click="amount -= 1"
+        >-</span
+      >
       <input class="flex-3/12 text-right w-full outline-0" v-model.number="amount" />
       <span class="flex-5/12 text-left select-none">&nbsp;{{ amount == 1 ? "pot" : "potten" }}</span>
-      <span class="flex-2/12 text-center cursor-pointer select-none hover:bg-gray-100 transition-colors" v-on:click="amount += 1">+</span>
+      <span v-if="!Number.isInteger(amount)" class="flex-2/12"></span>
+      <span
+        v-else
+        class="flex-2/12 text-center cursor-pointer select-none hover:bg-gray-100 transition-colors"
+        v-on:click="amount += 1"
+        >+</span
+      >
     </div>
 
-    <a v-if="totalPrice > 0"
+    <a
+      v-if="totalPrice > 0"
       class="block text-center bg-green-600 hover:bg-green-700 shadow-sm transition-all text-white rounded-md py-4 px-1 my-8 font-semibold"
       :href
       >€ {{ totalPrice }} betalen met iDEAL</a
