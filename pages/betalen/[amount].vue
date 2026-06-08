e8cd525ae0808f7f732c16a30e75ac92ac735291
Wietse de Vries <wietsedv@proton.me>
2026-06-08 19:53:07 +0200

    add direct amount link

---
+++ pages/betalen/[amount].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute();
+const config = useRuntimeConfig();
+const price = config.public.price;
+
+const amount = Number(route.params.amount);
+const totalPrice = amount * price;
+
+if (amount > 0 && Number.isFinite(totalPrice)) {
+  await navigateTo(
+    `https://bunq.me/hunneg/?amount=${totalPrice}&paymentMethod=IDEAL`,
+    { external: true },
+  );
+}
+</script>
